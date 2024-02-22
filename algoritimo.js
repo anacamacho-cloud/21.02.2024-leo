a9032ea35a91e889783c9dfca591b7cb400b8992
ana.camacho <129320167+anacamacho-cloud@users.noreply.github.com>
2024-02-22 12:04:14 -0300

    Add files via upload

---
+++ algoritimo.js
@@ -0,0 +1,12 @@
+const calcular = () =>{
+    let numero1 = document.getElementsById("input1");
+    let numero2 = document.getElementsById("input2");
+    let resultado = document.getElementById("input3");
+
+    resultado.value = parseInt(numero1.value) + parseInt(numero2.value);
+}
+
+
+
+
+
